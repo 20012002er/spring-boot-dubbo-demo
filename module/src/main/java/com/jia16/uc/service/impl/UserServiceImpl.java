8cc0d2658ee2119e55973281bf8ebfa82cab871a
yandong <20012002er@gmail.com>
2017-07-06 18:39:25 +0800

    加入登陆方法

--- module/src/main/java/com/jia16/uc/service/impl/UserServiceImpl.java
+++ module/src/main/java/com/jia16/uc/service/impl/UserServiceImpl.java
@@ -3,10 +3,16 @@ package com.jia16.uc.service.impl;
 import com.jia16.uc.domain.User;
 import com.jia16.uc.mapper.UserMapper;
 import com.jia16.uc.service.inter.UserService;
+import com.jia16.uc.util.PasswordEncryption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.jia16.uc.util.PasswordEncryption.BCRYPT;
 
 /**
  * Created by lazyb on 2017/6/22.
@@ -23,5 +29,27 @@ public class UserServiceImpl implements UserService {
     public User findByPhone(String phone) {
         logger.info("findByPhone");
         return userMapper.findUserByPhone(phone);
+    }
+
+    @Override
+    public Map<String, Object> login(String phone, String password) {
+        Map<String, Object> res = new HashMap<>();
+        User user = userMapper.findUserByPhone(phone);
+        int code = 0;
+        String msg = "success";
+        if (user == null) {
+            code = 1;
+            msg = "phone is error";
+        } else {
+            if (!BCRYPT.check(password, user.getPassword())) {
+                code = 2;
+                msg = "password is error";
+            } else {
+                res.put("user", user);
+            }
+        }
+        res.put("code", code);
+        res.put("msg", msg);
+        return res;
     }
 }
--- module/src/main/java/com/jia16/uc/service/inter/UserService.java
+++ module/src/main/java/com/jia16/uc/service/inter/UserService.java
@@ -2,11 +2,15 @@ package com.jia16.uc.service.inter;
 
 import com.jia16.uc.domain.User;
 
+import java.util.Map;
+
 /**
  * Created by lazyb on 2017/6/22.
  */
 public interface UserService {
 
     User findByPhone(String phone);
+
+    Map<String, Object> login(String phone, String password);
 
 }
